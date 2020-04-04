a9dcebc545a767d1d1bf681637d84d3c3c856e8c
Michiel Dral <m.c.dral@gmail.com>
2020-04-04 17:09:33 +0200

    Make Canvas into hooks

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -4,7 +4,7 @@ import { mapValues, clamp } from "lodash";
 import { IsolateCoordinatesForElement } from "./IsolateCoordinatesForElement.js";
 import { Transformation2DMatrix } from "../Data/TransformationMatrix.js";
 
-const isWhole = (number) => number % 1 === 0;
+const isWhole = number => number % 1 === 0;
 
 let ZoomContext = React.createContext({ scale: 1 });
 
@@ -13,7 +13,7 @@ export let Unzoom = ({ children, ...props }) => {
     return (
       <ZoomContext.Consumer
         children={({ scale }) =>
-          children({ scale, ...mapValues(props, (x) => scale * x) })
+          children({ scale, ...mapValues(props, x => scale * x) })
         }
       />
     );
@@ -28,32 +28,21 @@ export let Unzoom = ({ children, ...props }) => {
   }
 };
 
-class Canvas extends Component {
-  static defaultProps = {
-    maxTranslation: {
-      // I don't yet see a reason to limit this...
-      x: Infinity,
-      y: Infinity,
-    },
-    // Took these from sketch
-    // https://sketchapp.com/docs/canvas/#zooming
-    minZoom: 0.01,
-    maxZoom: 256,
-    initialTranslation: {
-      x: 0,
-      y: 0,
-    },
-  };
+let Canvas = ({
+  transform,
+  onTransformChange,
+  onBackgroundClick,
+  children,
+  maxZoom,
+  minZoom,
+  maxTranslation,
+  initialTranslation,
+}) => {
+  let isolateRef = React.useRef(null);
+  let measureRef = React.useRef(null);
 
-  state = {
-    transform: new Transformation2DMatrix(),
-  };
-
-  isolateRef = null;
-
-  doTranslation = ({ deltaX, deltaY }) => {
-    this.props.onTransformChange(({ transform }) => {
-      const { maxTranslation: max, initialTranslation } = this.props;
+  let doTranslation = ({ deltaX, deltaY }) => {
+    onTransformChange(({ transform }) => {
       // 'Normalize' the translation speed, we multiply our deltas by the current scale,
       // so we pan slower at close zooms & faster at far zooms
       const {
@@ -72,8 +61,8 @@ class Canvas extends Component {
           })
         )
         .clampTranslation({
-          x: [-max.x, max.x],
-          y: [-max.y, max.y],
+          x: [-maxTranslation.x, maxTranslation.x],
+          y: [-maxTranslation.y, maxTranslation.y],
         });
 
       return {
@@ -82,14 +71,8 @@ class Canvas extends Component {
     });
   };
 
-  doZoom = ({ clientX, clientY, deltaY }) => {
-    this.props.onTransformChange(({ transform, zoom }) => {
-      const {
-        maxZoom,
-        minZoom,
-        maxTranslation,
-        initialTranslation,
-      } = this.props;
+  let doZoom = ({ clientX, clientY, deltaY }) => {
+    onTransformChange(({ transform, zoom }) => {
       let initial_transform = new Transformation2DMatrix({
         e: -initialTranslation.x,
         f: -initialTranslation.y,
@@ -97,7 +80,7 @@ class Canvas extends Component {
 
       const scale = 1 - (1 / 110) * deltaY;
 
-      let { top, left } = this.measureRef.getBoundingClientRect();
+      let { top, left } = measureRef.current.getBoundingClientRect();
       // NOTE So learnt now, need to apply the initial_transform AFTER the invert... which now
       // .... I think about it.. is quite obvious from the fact that it is reversed
       const click_inside_canvas = transform
@@ -143,105 +126,115 @@ class Canvas extends Component {
     });
   };
 
-  onWheel = (event) => {
-    event.preventDefault();
-    const { deltaX, deltaY } = event;
-    const { doTranslation, doZoom } = this;
+  let initial_transform = new Transformation2DMatrix({
+    e: -initialTranslation.x,
+    f: -initialTranslation.y,
+  });
 
-    // NOTE It might actually be that this `isWhole` thing makes
-    // .... For a more uniform experience for laptops that don't have
-    // .... propper touchpads (like we do 👩🏿‍💻 (Yes that is a black women in tech LOOK I AM SO PROGRESSIVE (VIRTUE SIGNALLING +10)))
-    // .... So we might have to try that out
-    // let is_pinch = !(isWhole(deltaX) && isWhole(deltaY));
-    let is_pinch = event.ctrlKey;
-    if (event.ctrlKey) {
-      doZoom(event);
-    } else {
-      doTranslation(event);
+  let invert = initial_transform.multiply(transform.inverse());
+
+  React.useEffect(() => {
+    if (measureRef.current == null) {
+      return;
     }
-  };
 
-  render() {
-    const {
-      onBackgroundClick,
-      children,
-      initialTranslation,
-      transform,
-    } = this.props;
+    measureRef.current.addEventListener("wheel", event => {
+      event.preventDefault();
+      const { deltaX, deltaY } = event;
 
-    let initial_transform = new Transformation2DMatrix({
-      e: -initialTranslation.x,
-      f: -initialTranslation.y,
+      // NOTE It might actually be that this `isWhole` thing makes
+      // .... For a more uniform experience for laptops that don't have
+      // .... propper touchpads (like we do 👩🏿‍💻 (Yes that is a black women in tech LOOK I AM SO PROGRESSIVE (VIRTUE SIGNALLING +10)))
+      // .... So we might have to try that out
+      // let is_pinch = !(isWhole(deltaX) && isWhole(deltaY));
+      let is_pinch = event.ctrlKey;
+      if (event.ctrlKey) {
+        doZoom(event);
+      } else {
+        doTranslation(event);
+      }
     });
+  }, [measureRef.current]);
 
-    let invert = initial_transform.multiply(transform.inverse());
-
-    return (
-      <ZoomContext.Provider value={{ scale: invert.getScale().x }}>
+  return (
+    <ZoomContext.Provider value={{ scale: invert.getScale().x }}>
+      <div
+        style={{
+          overflow: "hidden",
+          height: "100%",
+          width: "100%",
+        }}
+        ref={measureRef}
+        onMouseDown={e => {
+          // Only reset selected_item if the click is **only** on the canvas,
+          // and not actually on one of the divs inside
+          if (e.target === e.currentTarget) {
+            onBackgroundClick();
+          }
+        }}
+      >
         <div
+          ref={isolateRef}
           style={{
-            overflow: "hidden",
-            height: "100%",
-            width: "100%",
+            transform: `
+              ${initial_transform.inverse().toString()}
+              ${transform.toMatrix3D().toString()}
+            `,
+            transformStyle: `preserve-3d`,
+            transformOrigin: "0% 0%",
           }}
-          ref={(ref) => (this.measureRef = ref)}
-          onWheel={this.onWheel}
-          onMouseDown={(e) => {
-            // Only reset selected_item if the click is **only** on the canvas,
+          onMouseDown={e => {
+            // Only trigger onBackgroundClick if the click is **only** on the canvas,
             // and not actually on one of the divs inside
             if (e.target === e.currentTarget) {
               onBackgroundClick();
             }
           }}
         >
+          <IsolateCoordinatesForElement
+            element={isolateRef.current}
+            // NOTE Ik weet dat je het zo hebt gemaakt dat ik in principe
+            // .... mapCoords={invert.applyToCoords}
+            // .... kan doen maar dat vind ik hekka onleesbaar(der)
+            mapCoords={coords => invert.applyToCoords(coords)}
+          />
+
+          {/* A gray shape to show the bounds, also just for reference */}
           <div
-            ref={(ref) => (this.isolateRef = ref)}
             style={{
-              transform: `
-                ${initial_transform.inverse().toString()}
-                ${transform.toMatrix3D().toString()}
-              `,
-              transformStyle: `preserve-3d`,
-              transformOrigin: "0% 0%",
+              pointerEvents: "none",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              border: "solid 1px white",
+              borderRadius: 5,
+              transform: `translateX(-50%) translateY(-50%)`,
+              width: maxTranslation.x * 2,
+              height: maxTranslation.y * 2,
             }}
-            onMouseDown={(e) => {
-              // Only trigger onBackgroundClick if the click is **only** on the canvas,
-              // and not actually on one of the divs inside
-              if (e.target === e.currentTarget) {
-                onBackgroundClick();
-              }
-            }}
-          >
-            <IsolateCoordinatesForElement
-              element={this.isolateRef}
-              // NOTE Ik weet dat je het zo hebt gemaakt dat ik in principe
-              // .... mapCoords={invert.applyToCoords}
-              // .... kan doen maar dat vind ik hekka onleesbaar(der)
-              mapCoords={(coords) => invert.applyToCoords(coords)}
-            />
+          />
 
-            {/* A gray shape to show the bounds, also just for reference */}
-            <div
-              style={{
-                pointerEvents: "none",
-                position: "absolute",
-                top: 0,
-                left: 0,
-                border: "solid 1px white",
-                borderRadius: 5,
-                transform: `translateX(-50%) translateY(-50%)`,
-                width: this.props.maxTranslation.x * 2,
-                height: this.props.maxTranslation.y * 2,
-              }}
-            />
-
-            {/* Render canvas items or whatever */}
-            {children}
-          </div>
+          {/* Render canvas items or whatever */}
+          {children}
         </div>
-      </ZoomContext.Provider>
-    );
-  }
-}
+      </div>
+    </ZoomContext.Provider>
+  );
+};
+Canvas.defaultProps = {
+  maxTranslation: {
+    // I don't yet see a reason to limit this...
+    x: Infinity,
+    y: Infinity,
+  },
+  // Took these from sketch
+  // https://sketchapp.com/docs/canvas/#zooming
+  minZoom: 0.01,
+  maxZoom: 256,
+  initialTranslation: {
+    x: 0,
+    y: 0,
+  },
+};
 
 export default Canvas;
