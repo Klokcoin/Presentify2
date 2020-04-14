3209dc6cf19e01a3b8fb996cbfee7b2b470eb122
Jonas <tuberjonas@gmail.com>
2020-04-14 22:58:54 +0200

    omgggg almost

--- src/Components/LayerList.js
+++ src/Components/LayerList.js
@@ -6,6 +6,13 @@ import styled, { css } from "styled-components/macro";
 const List = styled.div`
   display: flex;
   flex-direction: column;
+
+  flex-grow: 1;
+
+  background: ${(props) => (props.draggingOver ? "SeaGreen" : null)};
+  border: 1px solid red;
+  padding: 5px;
+  // z-index: ${(props) => props.level};
 `;
 
 let EditableName = styled.input`
@@ -33,7 +40,7 @@ const LayerItem = styled.div`
   cursor: pointer !important;
 
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
 
   padding: 12px 16px;
@@ -54,6 +61,11 @@ const LayerItem = styled.div`
     `};
 `;
 
+const NestedListContainer = styled.div`
+  width: 100%;
+  // border: 1px solid red;
+`;
+
 const Layer = ({
   item,
   index,
@@ -61,10 +73,20 @@ const Layer = ({
   remove_item,
   change_item,
   selected,
+  children,
+  set_draggingOverId,
+
+  isGroup,
 }) => {
   const [input_enabled, set_input_enabled] = React.useState(false);
   const [hovering, set_hovering] = React.useState(false);
   const [input, set_input] = React.useState(item.name);
+
+  // let [dragState, set_dragState] = useState({
+  //   isDragging: false,
+  //   draggingOver: null,
+  // });
+
   let inputRef = React.useRef(null);
 
   React.useEffect(() => {
@@ -74,67 +96,75 @@ const Layer = ({
     }
   }, [input_enabled]);
 
-  return (
-    <Draggable draggableId={item.id} index={index}>
-      {(provided, snapshot) => (
-        <LayerItem
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          onDoubleClick={() => set_input_enabled(true)}
-          onClick={() => select_item(item.id)}
-          onMouseEnter={() => set_hovering(true)}
-          onMouseLeave={() => set_hovering(false)}
-          dragging={snapshot.isDragging}
-          hovering={hovering}
-          selected={selected}
-        >
-          {input_enabled ? (
-            <EditableName
-              value={input}
-              disabled={!input_enabled}
-              onClick={(e) => e.preventDefault()}
-              ref={inputRef}
-              onBlur={() => {
-                set_input_enabled(false);
-                change_item(item.id, { name: input });
-              }}
-              onChange={(event) => set_input(event.target.value)}
-              onKeyUp={(event) => {
-                // If we pressed the enter key
-                if (event.keyCode === 13) {
-                  inputRef.current.blur();
-                }
-              }}
-            />
-          ) : (
-            <EllipsisOverflow
-              style={{
-                height: "100%",
-                width: "100%",
-                position: "relative",
-              }}
-            >
-              {item.name}
+  // useEffect(() => {}, [dragState]);
 
-              {hovering && (
-                <TrashBin
-                  title="Delete layer"
-                  onClick={(event) => {
-                    event.preventDefault();
-                    remove_item(item.id);
-                  }}
-                  style={{
-                    cursor: "pointer",
-                  }}
-                >
-                  <i className="fas fa-trash-alt"></i>
-                </TrashBin>
-              )}
-            </EllipsisOverflow>
-          )}
-        </LayerItem>
-      )}
+  return (
+    <Draggable draggableId={item.id} index={index} isDropDisabled>
+      {(provided, snapshot) => {
+        return (
+          <LayerItem
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            onDoubleClick={() => set_input_enabled(true)}
+            onClick={() => select_item(item.id)}
+            onMouseEnter={() => set_hovering(true)}
+            onMouseLeave={() => set_hovering(false)}
+            dragging={snapshot.isDragging}
+            hovering={hovering}
+            selected={selected}
+          >
+            {input_enabled ? (
+              <EditableName
+                value={input}
+                disabled={!input_enabled}
+                onClick={(e) => e.preventDefault()}
+                ref={inputRef}
+                onBlur={() => {
+                  set_input_enabled(false);
+                  change_item(item.id, { name: input });
+                }}
+                onChange={(event) => set_input(event.target.value)}
+                onKeyUp={(event) => {
+                  // If we pressed the enter key
+                  if (event.keyCode === 13) {
+                    inputRef.current.blur();
+                  }
+                }}
+              />
+            ) : (
+              <EllipsisOverflow
+                style={{
+                  height: "100%",
+                  width: "100%",
+                  position: "relative",
+                }}
+              >
+                {item.name}
+
+                {hovering && (
+                  <TrashBin
+                    title="Delete layer"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      remove_item(item.id);
+                    }}
+                    style={{
+                      cursor: "pointer",
+                    }}
+                  >
+                    <i className="fas fa-trash-alt"></i>
+                  </TrashBin>
+                )}
+              </EllipsisOverflow>
+            )}
+
+            {/* possible recursive list items, when this layer is a layerGroup */}
+
+            {children && <NestedListContainer>{children}</NestedListContainer>}
+          </LayerItem>
+        );
+      }}
     </Draggable>
   );
 };
@@ -148,6 +178,33 @@ const LayerList = ({
   change_item,
 }) => {
   let reversed_items = [...items].reverse();
+
+  let [destinationId, set_destinationId] = useState(null);
+
+  let [focusList, set_focusList] = useState(null);
+
+  function setGroupTree(list) {
+    list.forEach((i) => {
+      if (i.groupItems && i.groupItems.length > 0) {
+        groupTree[i.id] = { isGroup: true };
+        setGroupTree(i.groupItems);
+      } else groupTree[i.id] = { isGroup: false };
+    });
+  }
+
+  let groupTree = {};
+  setGroupTree(items);
+
+  // useEffect(() => {
+  //   if (items) {
+  //     console.log(items);
+  //     setGroupTree(items);
+  //   }
+  // }, [items]);
+
+  // useEffect(() => {
+  //   if (!combineState.dragId) console.log("uncombined");
+  // }, [combineState]);
 
   // Since we are reversing the array, this way we can get the index in the original, unreversed items (for drag ordering)
   const original_index = React.useCallback(
@@ -164,8 +221,33 @@ const LayerList = ({
     console.log("item_id", item_id);
   };
 
+  const onDragUpdate = (update) => {
+    let { combine, source, destination } = update;
+
+    // can change a destination list when trying to combine
+    if (combine) {
+      // console.log("combine", update);
+      // console.log("tree", groupTree);
+
+      let { draggableId } = combine;
+
+      //check if the item is combinable
+      let combinable = groupTree[draggableId].isGroup;
+      if (combinable) set_destinationId(draggableId);
+    } else {
+      // console.log("uncombine", update);
+
+      if (destination) set_destinationId(destination.droppableId);
+    }
+  };
+
   const onDragEnd = (result) => {
     const { source, destination } = result;
+
+    if (result.combine) {
+      console.log("trying to combine");
+    }
+
     // dropped outside the layer list
     if (!destination) {
       return;
@@ -183,26 +265,117 @@ const LayerList = ({
   };
 
   return (
-    <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
-      <Droppable droppableId="layer_list">
-        {(provided) => (
-          <List ref={provided.innerRef} {...provided.droppableProps}>
-            {reversed_items.map((item, index) => (
-              <Layer
-                item={item}
-                index={index}
-                key={item.id}
-                select_item={select_item}
-                remove_item={remove_item}
-                change_item={change_item}
-                selected={selected_id === item.id}
-              />
-            ))}
+    <DragDropContext
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      onDragUpdate={onDragUpdate}
+    >
+      <RecursiveList
+        level={0}
+        focusList={focusList}
+        set_focusList={set_focusList}
+        listId="base_list"
+        destinationId={destinationId}
+        items={reversed_items}
+        selected_id={selected_id}
+        select_item={select_item} //handleItem
+        remove_item={remove_item} //handleItem
+        change_item={change_item} //handleItem
+      />
+    </DragDropContext>
+  );
+};
+
+let RecursiveList = (props) => {
+  let {
+    items,
+    level,
+    listId,
+    destinationId,
+    selected_id,
+    focusList,
+    set_focusList,
+    ...handleItem
+  } = props;
+  const [isDropDisabled, set_isDropDisabled] = useState(false);
+
+  // useEffect(() => {
+  //   // let { dragId, dropId } = combineState;
+
+  //   if (!destinationId) set_isDropDisabled(false);
+  //   else {
+  //     if (destinationId === listId) set_isDropDisabled(false);
+  //     else set_isDropDisabled(true);
+  //   }
+  // }, [destinationId]);
+
+  useEffect(() => {
+    // let { dragId, dropId } = combineState;
+
+    if (!focusList) set_isDropDisabled(false);
+    else {
+      if (focusList === listId) set_isDropDisabled(false);
+      else set_isDropDisabled(true);
+    }
+  }, [focusList]);
+
+  return (
+    <Droppable
+      droppableId={listId}
+      isDropDisabled={isDropDisabled}
+      // isCombineEnabled
+      style={{ position: "absolute", zIndex: level || 0 }}
+    >
+      {(provided, snapshot) => {
+        return (
+          <List
+            style={{ pointerEvents: "auto" }} // to re-enable mouseOver
+            onMouseOver={(e) => {
+              console.log("over ", listId);
+              e.stopPropagation();
+              set_focusList(listId);
+            }}
+            onMouseLeave={(e) => {
+              // e.stopPropagation();
+              set_focusList(null);
+            }}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            draggingOver={snapshot.isDraggingOver}
+            level={level || 0}
+          >
+            {items.map((item, index) => {
+              let isGroup = item.groupItems && item.groupItems.length > 0;
+
+              return (
+                <Layer
+                  item={item}
+                  index={index}
+                  key={item.id}
+                  selected={selected_id === item.id}
+                  isGroup={isGroup}
+                  {...handleItem}
+                >
+                  {isGroup && (
+                    <RecursiveList
+                      level={level + 15000}
+                      listId={item.id}
+                      items={item.groupItems}
+                      selected_id={selected_id}
+                      destinationId={destinationId}
+                      focusList={focusList}
+                      set_focusList={set_focusList}
+                      {...handleItem}
+                    />
+                  )}
+                </Layer>
+              );
+            })}
             {provided.placeholder}
           </List>
-        )}
-      </Droppable>
-    </DragDropContext>
+        );
+      }}
+    </Droppable>
   );
 };
 
